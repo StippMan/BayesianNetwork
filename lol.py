b9585950e7d05f7c6aa159eb0a898469574a818f
Gwanke <ra91671@uem.br>
2021-03-03 18:42:02 -0300

    Funcionando com as tres coisas q ele quer la,as tres perguntas. Talvez precise adicionar uma analise de resultados(possivelmente com predict).

--- lol.py
+++ lol.py
@@ -11,22 +11,26 @@ def read_input():
 	return entrada
 
 def Model_def(dataFrame):
-	predict_data = dataFrame[:2500]
-	train_data = dataFrame[2500:]
+	dataFrame.drop(['blueKills','blueDeaths','blueEliteMonsters','blueTotalGold','blueTotalMinionsKilled','blueGoldPerMin',
+					'redEliteMonsters','redDragons','redTotalGold','redTotalExperience','redTotalMinionsKilled','redGoldPerMin'], axis=1, inplace=True)
+	dataFrame['blueTotalExperience'] = pd.cut(x=dataFrame['blueTotalExperience'], bins=[0,11000,14000,17000,30000],labels = ['pouco',
+																										   'media','acima-media','muito']) 
+	#print(dataFrame.info())
+	train_data = dataFrame[:7500]
+	predict_data = dataFrame[7500:]
+	#print(predict_data.info(),train_data.info())
 	model = BM([('blueFirstBlood','blueWins'),('blueTotalExperience','blueWins'),('blueDragons','blueWins')])
 	#Calculo de CPD com a estrategia MaximumLikehoodEstimator.
 	model.fit(train_data,estimator=MaximumLikelihoodEstimator)
 	#print(model.get_cpds('blueWins'))
 	#pra avaliacao final da predicao.
 	predict_data = predict_data.copy()
-	#predict_data.drop(['blueFirstBlood','blueTotalExperience','blueDragons',
-					   #'blueKills','blueDeaths','blueEliteMonsters','blueTotalGold','blueTotalMinionsKilled','blueGoldPerMin',
-					   #'redEliteMonsters','redDragons','redTotalGold','redTotalExperience','redTotalMinionsKilled','redGoldPerMin'], axis=1, inplace=True)
 	predict_data.drop('blueWins', axis=1, inplace=True)
+	#print(predict_data.shape)
+	#print(predict_data.head(10))
 	#print(predict_data.info())
-	y_pred = model.predict(predict_data)
+	#y_pred = model.predict_probability(predict_data)
 	#inferenciando
-	print(y_pred['blueWins'])
 	modelF = VariableElimination(model)
 	return modelF
 
@@ -34,14 +38,14 @@ def Model_def(dataFrame):
 def main():	
 	df=read_input()
 	modelo = Model_def(df)	
-	#print('\n Probabilidade do time Azul ganhar,caso o time vermelho de first blood. ')
-	#q1=modelo.query(variables=['blueWins'],evidence={'blueFirstBlood':0})
-	#print(q1)
-	#print('\n Probabilidade do time Azul ganhar,caso o time azul de first blood e tenha mais dragoes: ')
-	#q2=modelo.query(variables=['blueWins'],evidence={('blueFirstBlood'):1 , ('blueDragons'):1})
-	#print(q2)
-	#print('\n Probabilidade do time Azul ganhar,caso o time azul de first blood , tenha menos xp e menos dragoes ')
-	#q3=modelo.query(variables=['blueWins'],evidence={('blueFirstBlood'):1 , ('blueDragons'):0 ,('blueTotalExperience'):0 })
-	#print(q3)
+	print('\n Probabilidade do time Azul ganhar,caso o time vermelho de first blood. ')
+	q1=modelo.query(variables=['blueWins'],evidence={'blueFirstBlood':0})
+	print(q1)
+	print('\n Probabilidade do time Azul ganhar,caso o time azul de first blood e tenha um dragao: ')
+	q2=modelo.query(variables=['blueWins'],evidence={('blueFirstBlood'):1 , ('blueDragons'):1})
+	print(q2)
+	print('\n Probabilidade do time Azul ganhar,caso o time azul nao de first blood , muita xp e nenhum dragao ')
+	q3=modelo.query(variables=['blueWins'],evidence={('blueFirstBlood'):1 , ('blueDragons'):0,('blueTotalExperience'):'muito' })
+	print(q3)
 if __name__ == '__main__':
     main()
